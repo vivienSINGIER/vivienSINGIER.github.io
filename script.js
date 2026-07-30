c73bfb7e363d1970bf87a9feed1caef80378eec9
Vivien Singier <vsingier@gaming.tech>
2026-07-30 23:50:41 +0200

    [ADD] Scroll animations & nav bar update

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ function copyEmail() {
 }
 
 document.querySelector('.copy-email').addEventListener('click', copyEmail);
+
+const revealObs = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+            } else {
+                entry.target.classList.remove("in-view");
+            }
+        });
+    }, { threshold: 0.10 }
+);
+
+const revealElements = document.querySelectorAll(".reveal")
+ 
+revealElements.forEach((rEl) => revealObs.observe(rEl));
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+console.log(sections.length);
+console.log(navLinks.length);
+
+if (sections.length !== 0 && navLinks.length !== 0) {
+    const navObs = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const id = entry.target.getAttribute("id");
+                    navLinks.forEach(link => {
+                        link.classList.toggle("active", link.getAttribute("href") === `#${id}`);
+                    });
+                }
+            });
+        }, { rootMargin: "-45% 0px -45% 0px" }
+    ); 
+    
+    sections.forEach(sect => navObs.observe(sect));
+}
